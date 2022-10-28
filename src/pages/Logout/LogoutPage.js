1075367ec694bfcad95c47bdf9f07848005e0f76
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-10-27 21:28:14 -0700

    Handled logging out which removes tokens for localStorage

--- src/pages/Logout/LogoutPage.js
+++ src/pages/Logout/LogoutPage.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+
+const removeLocalStorageTokens = () => {
+  localStorage.removeItem('accessToken');
+  localStorage.removeItem('userToken');
+};
+
 const LogoutPage = () => {
+  useEffect(() => {
+    removeLocalStorageTokens();
+  }, []);
   return (
     <div>
       <h1>Logout Page</h1>

252b8db0551a6b15194d547febd38790a9a26135
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-11-10 09:10:02 -0800

    Added password strength meter. Not finished

--- src/pages/Register/RegisterPage.js
+++ src/pages/Register/RegisterPage.js
@@ -10,6 +10,7 @@ const RegisterPage = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [weaknesses, setWeaknesses] = useState({});
 
   const navigate = useNavigate();
 
@@ -36,6 +37,103 @@ const RegisterPage = () => {
     }
   };
 
+  const strengthMeter = document.getElementById('strengthMeter');
+  const passwordInput = document.getElementById('passwordInput');
+  const reasonsContainer = document.getElementById('reasons');
+
+  const handleStrengthMeter = (password) => {
+    setWeaknesses(calculatePasswordStrength(password));
+    let strength = 100;
+
+    console.log(reasonsContainer);
+    reasonsContainer.innterHTML = ''; //not working
+
+    weaknesses.forEach((weakness) => {
+      strength -= weakness.deduction;
+
+      const messageElement = document.createElement('div');
+      messageElement.innerText = weakness.message;
+      reasonsContainer.appendChild(messageElement);
+    });
+
+    strengthMeter.style.setProperty('--strength', strength);
+  };
+
+  const calculatePasswordStrength = (password) => {
+    const weaknesses = [];
+    weaknesses.push(lengthWeakness(password));
+    weaknesses.push(lowercaseWeakness(password));
+    weaknesses.push(uppercaseWeakness(password));
+    weaknesses.push(numberWeakness(password));
+    weaknesses.push(specialCharacterWeakness(password));
+
+    return weaknesses;
+  };
+
+  const lengthWeakness = (password) => {
+    const length = password.length;
+
+    if (length <= 5) {
+      return {
+        message: 'Your password is too short',
+        deduction: 40,
+      };
+    }
+
+    if (length <= 10) {
+      return {
+        message: 'Your password could be longer',
+        deduction: 15,
+      };
+    }
+  };
+
+  const lowercaseWeakness = (password) => {
+    return characterTypeWeaknessHelper(
+      password,
+      /[a-z]/g,
+      'lowercase characters'
+    );
+  };
+
+  const uppercaseWeakness = (password) => {
+    return characterTypeWeaknessHelper(
+      password,
+      /[A-Z]/g,
+      'uppercase characters'
+    );
+  };
+
+  const numberWeakness = (password) => {
+    return characterTypeWeaknessHelper(password, /[0-9]/g, 'number characters');
+  };
+
+  const specialCharacterWeakness = (password) => {
+    return characterTypeWeaknessHelper(
+      password,
+      /[^0-9a-zA-Z\s]/g,
+      'special characters'
+    );
+  };
+
+  const characterTypeWeaknessHelper = (password, regex, type) => {
+    const matches = password.match(regex) || [];
+
+    if (matches.length === 0) {
+      return {
+        message: `Your password has no ${type}`,
+        deduction: 20,
+      };
+    }
+
+    if (matches.length <= 2) {
+      return {
+        message: `Your password could use more ${type}`,
+        deduction: 5,
+      };
+    }
+  };
+
   return (
     <div>
       <h1>Register</h1>
@@ -44,43 +142,57 @@ const RegisterPage = () => {
         <label htmlFor='firstName'>First Name</label>
         <input
           id='firstName'
+          className={styles.inputField}
           type='text'
           onChange={async (event) => {
             await setFirstName(event.target.value);
           }}
-        ></input>
+          autoFocus
+          aria-labelledby='first name'
+        />
         <label htmlFor='lastName'>Last Name</label>
         <input
           id='lastName'
+          className={styles.inputField}
           type='text'
           onChange={async (event) => {
             await setLastName(event.target.value);
           }}
-        ></input>
+          aria-labelledby='last name'
+        />
         <label htmlFor='username'>Username</label>
         <input
           id='username'
+          className={styles.inputField}
           type='text'
           onChange={async (event) => {
             await setUsername(event.target.value);
           }}
-        ></input>
+          aria-labelledby='username'
+        />
         <label htmlFor='email'>Email</label>
         <input
           id='email'
+          className={styles.inputField}
           type='text'
           onChange={async (event) => {
             await setEmail(event.target.value);
           }}
-        ></input>
+          aria-labelledby='email'
+        />
         <label htmlFor='password'>Password</label>
         <input
           id='password'
+          className={styles.inputField}
           type='password'
           onChange={async (event) => {
             await setPassword(event.target.value);
+            handleStrengthMeter(event.target.value);
           }}
-        ></input>
+          aria-labelledby='password'
+        />
+        <div id='strengthMeter' className={styles.strengthMeter}></div>
+        <div id='reasons' className={styles.reasons}></div>
         <br />
         <button onClick={(e) => handleSubmit(e)}>Register</button>
       </form>

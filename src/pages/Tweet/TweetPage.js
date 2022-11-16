9358cb7687165005c69d2fa6fb63ea46212e5aa3
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-11-15 21:55:02 -0800

    Updated Tweet page

--- src/pages/Tweet/TweetPage.js
+++ src/pages/Tweet/TweetPage.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 import { baseUrl } from '../../config';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 const TweetPage = () => {
   const [tweet, setTweet] = useState('');
+  const { user, setUser } = useContext(UserContext);
+
   const handleTweetSubmit = (e) => {
     e.preventDefault();
     console.log('handle tweet submit clicked');
-    // axios.post(baseUrl + '/tweet', {
-    //   tweet: 'hello world 1',
-    // });
+
+    console.log(tweet);
+    axios.post(baseUrl + '/tweet', {
+      tweet: 'hello world 1',
+      username: user.username,
+    });
   };
 
   useEffect(() => {
     console.log(tweet);
+    console.log(user.username);
   }, [tweet]);
 
   return (

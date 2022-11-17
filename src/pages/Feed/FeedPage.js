326baf301fc3f8f02bfc40c20d9c26bfd4c6a687
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-11-17 10:26:48 -0800

    Added tweet functionality and displayed it on FeedPage

--- src/pages/Feed/FeedPage.js
+++ src/pages/Feed/FeedPage.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import axios from 'axios';
+import { baseUrl } from '../../config';
+import { useEffect, useState } from 'react';
+import styles from './FeedPage.module.css';
 
 const FeedPage = () => {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    getAllTweets();
+  }, []);
+
+  const deleteAllTweets = async () => {
+    console.log('got here');
+    const response = await axios.delete(baseUrl + '/tweet/delete-all');
+    console.log(response, 'response');
+    if (response.status === 200) {
+      getAllTweets();
+    } else {
+      document.getElementById('delete-error-msg').innerHTML =
+        'something went wrong';
+    }
+  };
+
+  const getAllTweets = async () => {
+    const response = await axios.get(baseUrl + '/tweets');
+    setTweets(response.data);
+    console.log(response, 'all tweets');
+  };
+
   return (
     <div>
       <h1>Feed</h1>
       <p>This is the feed page</p>
+      <br />
+      <br />
+      <br />
+      {tweets.map((tweet) => (
+        <div className={styles.tweet}>
+          {tweet.tweet}
+          <br />
+          <div className={styles.postedBy}>
+            posted by {tweet.username} [{tweet.createdAt}]
+          </div>
+        </div>
+      ))}
+
+      <button
+        onClick={() => {
+          deleteAllTweets();
+        }}
+      >
+        Delete All Tweets
+      </button>
+      <br />
+      <div id='delete-error-msg'></div>
     </div>
   );
 };
--- src/pages/Tweet/TweetPage.js
+++ src/pages/Tweet/TweetPage.js
@@ -7,15 +7,26 @@ const TweetPage = () => {
   const [tweet, setTweet] = useState('');
   const { user, setUser } = useContext(UserContext);
 
-  const handleTweetSubmit = (e) => {
+  const handleTweetSubmit = async (e) => {
     e.preventDefault();
     console.log('handle tweet submit clicked');
 
     console.log(tweet);
-    axios.post(baseUrl + '/tweet', {
-      tweet: 'hello world 1',
+    let response = await axios.post(baseUrl + '/tweet', {
+      tweet: tweet,
       username: user.username,
     });
+
+    console.log(response.status);
+    console.log(response.data.tweet);
+    console.log(response.data.username);
+
+    if (response.status === 200) {
+      document.getElementById('tweetText').value = '';
+      document.getElementById(
+        'tweetSuccess'
+      ).textContent = `You have successfully tweeted: ${tweet}`;
+    }
   };
 
   useEffect(() => {
@@ -30,6 +41,7 @@ const TweetPage = () => {
 
       <form>
         <input
+          id='tweetText'
           type='text'
           onChange={(event) => {
             setTweet(event.target.value);

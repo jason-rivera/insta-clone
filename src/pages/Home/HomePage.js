018a843665181fb532ede6658eb546e7c55355cc
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-12-02 16:24:29 -0800

    Clean up

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,40 +1,30 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { UserContext } from '../../UserContext';
 
 const HomePage = () => {
   const navigate = useNavigate();
-  // const { user, setUser } = useContext(UserContext);
+
   const { user, setUser } = useContext(UserContext);
 
-  // useEffect(() => {}, []);
+  useEffect(() => {}, []);
 
   const handleViewUsersBtn = () => {
     navigate('/users');
   };
-  // const handleLoginBtn = () => {
-  //   navigate('/login');
-  // };
-  // const handleSignupBtn = () => {
-  //   navigate('/register');
-  // };
-
-  const setCookie = async () => {
-    // console.log('set cookie test');
-    // const response = await axios.get(baseUrl + '/setCookie');
-    // console.log(response);
-
-    document.cookie = 'TEST=1; expires=Tue, 14 Oct 2014 20:23:32 GMT; path=/';
-    console.log(document.cookie);
+  const handleLoginBtn = () => {
+    navigate('/login');
+  };
+  const handleSignupBtn = () => {
+    navigate('/register');
   };
 
   return (
     <>
       <h1>Home Page</h1>
+      {user && <p>Hello {user.username}</p>}
       <p>This is the home page</p>
-      {/* <p>context {user.username}</p> */}
+
       <br />
       <button onClick={() => handleViewUsersBtn()}>
         View All Current Users
@@ -43,7 +33,7 @@ const HomePage = () => {
       <br />
       <br />
       <br />
-      {/* {!user && (
+      {!user && (
         <>
           <button onClick={() => handleLoginBtn()}>Login</button>
           <br />
@@ -52,17 +42,7 @@ const HomePage = () => {
             Not A Member? Sign Up!
           </button>
         </>
-      )} */}
-
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          setCookie();
-        }}
-      >
-        set cookie
-      </button>
+      )}
     </>
   );
 };
--- src/pages/Profile/ProfilePage.js
+++ src/pages/Profile/ProfilePage.js
@@ -3,32 +3,12 @@ import { getUserByUsername } from '../../api/usersAPI';
 import axios from 'axios';
 import { baseUrl } from '../../config';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 
 const ProfilePage = () => {
-  const [user, setUser] = useState({});
+  const { user, setUser } = useContext(UserContext);
 
-  useEffect(() => {
-    getUserInfo();
-  }, []);
-
-  const getUserInfo = async () => {
-    const response = await getUserByUsername(localStorage.getItem('userToken'));
-    console.log(response);
-    setUser(response);
-  };
-
-  // const handleEditUsername = async () => {};
-
-  // const handleEditFirstName = async () => {
-  //   const response = await axios.patch(baseUrl + '/user/update-first-name');
-  //   console.log(response);
-  // };
-
-  // const handleEditLastName = async () => {};
-
-  // const handleEditEmail = async () => {};
-
-  // const handleEditPassword = async () => {};
+  useEffect(() => {}, []);
 
   return (
     <div>
@@ -40,24 +20,11 @@ const ProfilePage = () => {
       <div>First Name: {user.firstName}</div>
       <div>Last Name: {user.lastName}</div>
       <div>Email: {user.email}</div>
-      {/* <div>Password: {user.password}</div> */}
+
       <br />
       <Link to='/profile/edit'>
         <button>Edit Profile</button>
       </Link>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          getUserInfo();
-        }}
-      >
-        get user info
-      </button>
     </div>
   );
 };

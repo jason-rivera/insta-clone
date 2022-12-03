43465d984aedc5e208cfe9b8709949342ef7ed16
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2022-12-02 17:26:24 -0800

    handle redirecting to login if JWT is expired

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,9 +1,26 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../UserContext';
+import { useEffect } from 'react';
+
+import { verifyCurrentUser } from '../api/verifyAPI';
 
 const RequireAuth = () => {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    handleVerification();
+  }, []);
+
+  const handleVerification = async () => {
+    try {
+      const response = await verifyCurrentUser();
+      console.log(response, 'response from handleVerification');
+    } catch (e) {
+      console.log(e, 'from handleVerification');
+      setUser(null);
+    }
+  };
 
   console.log('checking in RequireAuth');
 

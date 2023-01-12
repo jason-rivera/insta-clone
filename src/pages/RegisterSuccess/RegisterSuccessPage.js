e155305231750d37c939f30aa319c85d061276c5
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-01-12 13:18:18 -0800

    Updated styling of RegisterSuccessPage

--- src/pages/RegisterSuccess/RegisterSuccessPage.js
+++ src/pages/RegisterSuccess/RegisterSuccessPage.js
@@ -1,6 +1,8 @@
+import styles from './RegisterSuccessPage.module.css';
+
 const RegisterSuccessPage = () => {
   return (
-    <div>
+    <div className={styles.registerSuccessPageContainer}>
       <h1>Register Success Page</h1>
       <p>You have successfully registered your account. Please login.</p>
     </div>

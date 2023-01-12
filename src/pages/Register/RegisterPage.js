5755c1be682e457af364f305e8e299202da695ed
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-01-12 14:55:27 -0800

    Removed Strength Meter in RegisterPage.js and added remove picture button

--- src/pages/Register/RegisterPage.js
+++ src/pages/Register/RegisterPage.js
@@ -12,7 +12,6 @@ const RegisterPage = () => {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [weaknesses, setWeaknesses] = useState([]);
 
   const navigate = useNavigate();
 
@@ -76,99 +75,6 @@ const RegisterPage = () => {
     });
   };
 
-  const strengthMeter = document.getElementById('strengthMeter');
-  const passwordInput = document.getElementById('passwordInput');
-
-  const handleStrengthMeter = async (password) => {
-    // const reasonsContainer = document.getElementById('reasons');
-    // reasonsContainer.innerHTML = '';
-    // await setWeaknesses(calculatePasswordStrength(password));
-    // let strength = 100;
-    // weaknesses.forEach((weakness) => {
-    //   console.log(weakness.deduction);
-    //   strength -= weakness.deduction;
-    //   const messageElement = document.createElement('div');
-    //   messageElement.innerText = weakness.message;
-    //   reasonsContainer.appendChild(messageElement);
-    // });
-    // strengthMeter.style.setProperty('--strength', strength);
-  };
-
-  const calculatePasswordStrength = (password) => {
-    const weaknesses = [];
-    weaknesses.push(lengthWeakness(password));
-    weaknesses.push(lowercaseWeakness(password));
-    weaknesses.push(uppercaseWeakness(password));
-    weaknesses.push(numberWeakness(password));
-    weaknesses.push(specialCharacterWeakness(password));
-
-    return weaknesses;
-  };
-
-  const lengthWeakness = (password) => {
-    const length = password.length;
-
-    if (length <= 2) {
-      return {
-        message: 'Your password is too short',
-        deduction: 40,
-      };
-    }
-
-    if (length <= 6) {
-      return {
-        message: 'Your password could be longer',
-        deduction: 15,
-      };
-    }
-  };
-
-  const lowercaseWeakness = (password) => {
-    return characterTypeWeaknessHelper(
-      password,
-      /[a-z]/g,
-      'lowercase characters'
-    );
-  };
-
-  const uppercaseWeakness = (password) => {
-    return characterTypeWeaknessHelper(
-      password,
-      /[A-Z]/g,
-      'uppercase characters'
-    );
-  };
-
-  const numberWeakness = (password) => {
-    return characterTypeWeaknessHelper(password, /[0-9]/g, 'number characters');
-  };
-
-  const specialCharacterWeakness = (password) => {
-    return characterTypeWeaknessHelper(
-      password,
-      /[^0-9a-zA-Z\s]/g,
-      'special characters'
-    );
-  };
-
-  const characterTypeWeaknessHelper = (password, regex, type) => {
-    const matches = password.match(regex) || [];
-
-    if (matches.length === 0) {
-      return {
-        message: `Your password has no ${type}`,
-        deduction: 20,
-      };
-    }
-
-    if (matches.length <= 2) {
-      return {
-        message: `Your password could use more ${type}`,
-        deduction: 5,
-      };
-    }
-  };
-
   return (
     <div className={styles.registerPageContainer}>
       <h1>Register</h1>
@@ -184,6 +90,15 @@ const RegisterPage = () => {
           }}
           aria-labelledby='avatar'
         />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            console.log('removing');
+            setAvatar('');
+          }}
+        >
+          Remove Profile Picture
+        </button>
         <label htmlFor='username'>Username</label>
         <input
           id='username'
@@ -232,14 +147,10 @@ const RegisterPage = () => {
           type='password'
           onChange={async (event) => {
             await setPassword(event.target.value);
-            await handleStrengthMeter(event.target.value);
           }}
           aria-labelledby='password'
         />
-        <div id='strengthMeter' className={styles.strengthMeter}></div>
-        <div id='reasons' className={styles.reasons}>
-          asdf
-        </div>
+
         <br />
         <button className={global.btn} onClick={(e) => handleSubmit(e)}>
           Register

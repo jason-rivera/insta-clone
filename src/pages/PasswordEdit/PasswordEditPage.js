4411ca88d035150b9fa796a536a6bba24c4a8f32
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-01-12 14:31:15 -0800

    Added Update Password logic

--- src/pages/PasswordEdit/PasswordEditPage.js
+++ src/pages/PasswordEdit/PasswordEditPage.js
@@ -1,26 +1,81 @@
 import React from 'react';
 import styles from './PasswordEditPage.module.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import { baseUrl } from '../../config';
+import { useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 const PasswordEditPage = () => {
+  const [currentPassword, setCurrentPassword] = useState();
+  const [newPassword, setNewPassword] = useState();
   const navigate = useNavigate();
+  const { user } = useContext(UserContext);
+
+  const handleUpdatePassword = async (e) => {
+    e.preventDefault();
+
+    document.getElementById('update-password-error-msg').innerHTML = '';
+
+    try {
+      const response = await axios.patch(
+        baseUrl + '/profile/password/update',
+        {
+          username: user.username,
+          currentPassword: currentPassword,
+          newPassword: newPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+          },
+        }
+      );
+
+      //if success navigate to success page
+      if (response.status === 200) {
+        navigate('/profile/password/success');
+        console.log('success 200');
+      }
+    } catch (e) {
+      console.error(e);
+      if (e.response.status === 401) {
+        //if there's an error, stay on page and display error message
+        document.getElementById('update-password-error-msg').innerHTML =
+          e.response.data.message;
+      }
+    }
+  };
 
   return (
     <div className={styles.passwordEditPageContainer}>
       <h1>Change Password</h1>
       <br />
-      Current Password: <input id='current-password-input' type='password' />
+      <form>
+        Current Password:{' '}
+        <input
+          id='current-password-input'
+          type='password'
+          onChange={(e) => setCurrentPassword(e.target.value)}
+        />
+        <br />
+        New Password:{' '}
+        <input
+          id='new-password-input'
+          type='password'
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={(e) => handleUpdatePassword(e)}>
+          Update Password
+        </button>
+      </form>
       <br />
-      New Password: <input id='new-password-input' type='password' />
-      <br />
-      <button onClick={() => navigate('/profile/password/success')}>
-        Update Password
-      </button>
       <br />
       <br />
       <br />
-      <br />
-      <div id='update-password-success-msg'></div>
+      <div id='update-password-error-msg'></div>
     </div>
   );
 };

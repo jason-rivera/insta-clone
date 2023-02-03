80feea760160efb8f7a104d05390dd2fadf1aba3
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-02-03 11:52:14 -0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const local = 'http://localhost:8080/api/v1';
 const heroku = 'https://git.heroku.com/twitter-clone-backend-app.git/api/v1';
-const herokuRelative = '/api/v1';
+const herokuRelative = '/api/v1asdf';
 
 const baseUrl = herokuRelative;
 
